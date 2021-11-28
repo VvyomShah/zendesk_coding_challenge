c471dd96b1d2c4af8a88ebff463386ff8511365c
oogashop@paralleldots.com <vvyom@paralleldots.com>
2021-11-28 15:13:59 -0500

    Added ticket pagination

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import requests
 import getpass
-import json
 import os
 
 import credentials
@@ -11,26 +10,21 @@ class Authenticate:
         self.SUBDOMAIN = None
         self.USERNAME = None
         self.PASSWORD = None
-        self.getCredentials()
+        # self.getCredentials()
 
-        # self.SUBDOMAIN = credentials.credentials['domain']
-        # self.USERNAME = credentials.credentials['email']
-        # self.PASSWORD = credentials.credentials['password']
+        self.SUBDOMAIN = credentials.credentials['domain']
+        self.USERNAME = credentials.credentials['email']
+        self.PASSWORD = credentials.credentials['password']
 
     def getCredentials(self):
-        get_username = getpass.getuser()
-        get_password = getpass.getpass()
-
-        try:
-            self.USERNAME = getpass.getpass('Username: ', get_username)
-
-        except Exception as error:
-            print('ERROR', error)
+        self.USERNAME = input('Username: ')
         
         try:
-            self.PASSWORD = getpass.getpass('Password: ', get_password)
+            self.PASSWORD = getpass.getpass('Password: ')
+            print(self.USERNAME, self.PASSWORD)
 
         except Exception as error:
+            print(self.USERNAME, self.PASSWORD)
             print('ERROR', error)
 
 
@@ -47,31 +41,52 @@ class Tickets:
         url = os.path.join(self.SUBDOMAIN, API)
         response = requests.get(url, auth=credentials, params=payload)
         if response.status_code != 200:
-            print('Error')
+            print(response.status_code)
             return 'Error'
         return response.json()
 
-    def getTickets(self, credentials):
-
-        if not self.after and not self.before and not self.has_more: 
+    def getTickets(self, credentials, change=0):
+        if change == 0: 
             ticketsData = self.request('api/v2/tickets.json', credentials=credentials, payload = {"page[size]": "25"})
-            self.has_more = ticketsData['meta']['has_more']
-            self.before = ticketsData['meta']['before_cursor']
-            self.after = ticketsData['meta']['after_cursor']
-            self.current_tickets = ticketsData['tickets']
-            print(self.current_tickets)
+        
+        elif change == 1:
+            ticketsData = self.request('api/v2/tickets.json', credentials=credentials, payload = {"page[size]": "25", "page[after]": self.after})
+            if ticketsData['meta']['has_more'] == False:
+                print('No more tickets to display.')
+                return
 
+        elif change == -1:
+            ticketsData = self.request('api/v2/tickets.json', credentials=credentials, payload = {"page[size]": "25", "page[before]": self.before})
+            if ticketsData['meta']['has_more'] == False:
+                print('No more previous tickets to display.')
+                return
 
-
-
+        self.has_more = ticketsData['meta']['has_more']
+        self.before = ticketsData['meta']['before_cursor']
+        self.after = ticketsData['meta']['after_cursor']
+        self.current_tickets = ticketsData['tickets']
+        
 
 class Display:
 
     def __init__(self):
         Auth = Authenticate()
         allTickets = Tickets()
+        credentials = (Auth.USERNAME, Auth.PASSWORD)
+        allTickets.getTickets(credentials)
+        self.displayMenu()
+    
+    def displayMenu(self):
+        while True:
+            print('(1) - Next page \n(2) - Previous page \n(3) - Select Ticket by Ticket ID \n')
+            choice = input('Enter Choice: ')
 
-        allTickets.getTickets((Auth.USERNAME, Auth.PASSWORD))
+            if int(choice) == 1:
+                self.allTickets.getTickets(credentials, change=1)
+            if int(choice) == 2:
+                self.allTickets.getTickets(credentials, change=-1)
+
+
 
 if __name__ == '__main__':
     view = Display()
